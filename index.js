7e537274632420ae2bedd127ab68daf34f89ec0c
Dave <Daverevell491@gmail.com>
2022-10-28 11:26:50 +0100

    JS now checks if tasks are complete/incomplete

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 const tasksUL = document.querySelector('#todo-list')
 
+
+// STATE
 const state = {
   tasks: []
 }
 
+
+// RENDER CODE
 function renderToDo() {
   tasksUL.innerHTML = ''
   state.tasks.forEach((task) => {
     const li = document.createElement('li')
     li.innerText = task.title
-    
+
     const complete = document.createElement('input')
     complete.setAttribute('type', 'checkbox')
     complete.addEventListener('click', (event) => {
-      console.log('checkbox clicked')
-      // if (state.tasks["completed"] === true) {
-      //   console.log('true')
-      // } else if (state.tasks["completed"] === false) {
-      //   console.log('false')
-      // }
-      if(event.target.click) {
-        checkCompleted()
+      if (task.completed === true) {
+        task.completed = false
+        console.log(task.title, "is now no longer completed")
+      } else if (task.completed === false) {
+        task.completed = true
+        console.log(task.title, "is now completed")
       }
     })
 
-    
+    if (task.completed === true) {
+      console.log(task.title, 'completed: true')
+      li.setAttribute('class', 'completed')
+      complete.checked = true
+    } else if (task.completed === false) {
+      console.log(task.title, 'completed: false')
+      li.removeAttribute('class', 'completed')
+      complete.checked = false
+    }
+
 
     li.appendChild(complete)
     tasksUL.appendChild(li)
   })
+}
+
+function checkboxClick() {
+  
 }
 
 function loadToDo() {
@@ -44,12 +59,5 @@ function loadToDo() {
     })
 }
 
-function checkCompleted() {
-  if (state.tasks['completed'] === true) {
-    console.log('true')
-  } else if (state.tasks['completed'] === false) {
-    console.log('false')
-  }
-}
-
+//INIT
 loadToDo()
